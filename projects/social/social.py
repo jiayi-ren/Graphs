61468a34eeb3d2bef944ae4046a8613040237c12
Jiayi Ren <jiayiren22@gmail.com>
2020-08-12 15:48:35 -0700

    complete social

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,12 +1,19 @@
+import random
+from collections import deque
+
 class User:
     def __init__(self, name):
         self.name = name
 
+    def __repr__(self):
+        return f"User: ({repr(self.name)})"
+
 class SocialGraph:
     def __init__(self):
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
+        # self.last_id = 0
+        # self.users = {}
+        # self.friendships = {}
+        self.reset()
 
     def add_friendship(self, user_id, friend_id):
         """
@@ -28,6 +35,11 @@ class SocialGraph:
         self.users[self.last_id] = User(name)
         self.friendships[self.last_id] = set()
 
+    def reset(self):
+        self.last_id = 0
+        self.users = {}
+        self.friendships = {}
+
     def populate_graph(self, num_users, avg_friendships):
         """
         Takes a number of users and an average number of friendships
@@ -39,14 +51,46 @@ class SocialGraph:
         The number of users must be greater than the average number of friendships.
         """
         # Reset graph
-        self.last_id = 0
-        self.users = {}
-        self.friendships = {}
+        # self.last_id = 0
+        # self.users = {}
+        # self.friendships = {}
+        self.reset()
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {i}")
 
         # Create friendships
+        possible_friendships = []
+        
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        random.shuffle(possible_friendships)
+
+        for i in range(num_users * avg_friendships // 2):
+            friendships = possible_friendships[i]
+            self.add_friendship(friendships[0], friendships[1])
+
+    def bfs(self, start, end):
+        q = deque()
+        q.append([start])
+
+        visited = set()
+
+        while len(q) > 0:
+            path = q.popleft()
+            v = path[-1]
+            if v not in visited:
+                if v == end:
+                    return path
+                visited.add(v)
+                for neighbor in list(self.friendships[v]):
+                    new_path = list(path)
+                    new_path.append(neighbor)
+                    q.append(new_path)
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,6 +103,22 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        # add itself
+        visited[user_id] = [user_id]
+        # add all direct friend
+        for f in list(self.friendships[user_id]):
+            visited[f] = [user_id, f]
+        # bfs on unvisited user
+        for u in list(self.users):
+            # if unvisited
+            if u not in visited:
+                # bfs(start, end, friends)
+                path = self.bfs(user_id, u)
+                # if path is valid
+                if path:
+                    # add path
+                    visited[u] = path
+
         return visited
 
 
